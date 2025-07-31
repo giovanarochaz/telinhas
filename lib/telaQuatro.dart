656b85be2d943933aca178bb5e759fff8ea5406f
Giovana Rocha <giovanarocha109@gmail.com>
2025-07-31 20:36:13 -0300

    tester

--- lib/telaQuatro.dart
+++ lib/telaQuatro.dart
@@ -4,7 +4,7 @@ class Tela4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Tela 4')),
+      appBar: AppBar(title: Text('Tela 45')),
       body: Center(child: Text('Conteúdo da Tela 4')),
     );
   }
